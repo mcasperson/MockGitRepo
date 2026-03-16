1a24386ec66476d9914cf0cf2bafc661c09a0773
Matthew Casperson <matthewcasperson@gmail.com>
2026-03-16 13:42:16 +1000

    Add salt to SHA-256 hashing for enhanced security

--- internal/domain/security/security.go
+++ internal/domain/security/security.go
@@ -5,7 +5,9 @@ import (
 	"encoding/hex"
 )
 
+const Salt = "%D+u1zM0ZnD#tQ}Y5*+m+b7:cGasZmt}"
+
 func GetSha256(input string) string {
-	hash := sha256.Sum256([]byte(input))
+	hash := sha256.Sum256([]byte(input + Salt))
 	return hex.EncodeToString(hash[:])
 }
